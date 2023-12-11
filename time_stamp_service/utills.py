0bdd57b317969f14dcc56178b537b51f853c2e7b
mmauryart <137374265+mmauryart@users.noreply.github.com>
2023-12-11 11:51:16 +0530

    Update utills.py

--- time_stamp_service/utills.py
+++ time_stamp_service/utills.py
@@ -4,25 +4,56 @@ from concurrent.futures import ThreadPoolExecutor
 import json
 
 def calculate_ssim(frame1, frame2):
+    """
+    Calculate the structural similarity between two frames.
+
+    :param frame1: First frame of the video.
+    :param frame2: Next frame of the video.
+    :return: Structural similarity score between the frames.
+    """
     gray_frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
     gray_frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
     score, _ = ssim(gray_frame1, gray_frame2, full=True)
     return score
 
 def process_frame(frame1, frame2, threshold):
+    """
+    Check if the structural similarity score between two frames is greater than the specified threshold.
+
+    :param frame1: First frame of the video.
+    :param frame2: Next frame of the video.
+    :param threshold: Minimum similarity score required.
+    :return: True if similarity score is greater than or equal to the threshold, False otherwise.
+    """
     similarity_score = calculate_ssim(frame1, frame2)
     return similarity_score >= threshold
 
 def format_time(seconds):
+    """
+    Convert seconds to a formatted time string (hh:mm:ss).
+
+    :param seconds: Time in seconds.
+    :return: Formatted time string.
+    """
     minutes, seconds = divmod(seconds, 60)
     hours, minutes = divmod(minutes, 60)
     return f"{int(hours):02d}:{int(minutes):02d}:{int(seconds):02d}"
 
 def detect_slide_durations(video_path, threshold=0.95, resize_factor=0.5, skip_frames=5):
+    """
+    Detect the durations of similar slides in a video.
+
+    :param video_path: Path to the video file.
+    :param threshold: Minimum similarity score required to consider frames as similar.
+    :param resize_factor: Resize factor for frames to speed up processing.
+    :param skip_frames: Number of frames to skip between comparisons.
+    :return: JSON data containing slide durations.
+    """
+    # Open the video file
     cap = cv2.VideoCapture(video_path)
     ret, reference_frame = cap.read()
 
-    # Resize reference frame
+    # Resize the reference frame
     reference_frame = cv2.resize(reference_frame, None, fx=resize_factor, fy=resize_factor)
 
     slide_durations = []
@@ -38,7 +69,7 @@ def detect_slide_durations(video_path, threshold=0.95, resize_factor=0.5, skip_f
             if not ret:
                 break
 
-            # Resize current frame
+            # Resize the current frame
             frame = cv2.resize(frame, None, fx=resize_factor, fy=resize_factor)
 
             # Perform frame comparison in parallel
@@ -54,6 +85,7 @@ def detect_slide_durations(video_path, threshold=0.95, resize_factor=0.5, skip_f
                     "duration": format_time(duration)
                 })
 
+                # Update reference frame and start time for the next slide
                 reference_frame = frame
                 start_time = end_time
 
@@ -63,5 +95,5 @@ def detect_slide_durations(video_path, threshold=0.95, resize_factor=0.5, skip_f
     return {"Slide_durations_Test": slide_durations}
 
 # Example usage
-#video_path = '2min_output_clip.mp4'
+# video_path = '2min_output_clip.mp4'
 # result_json = detect_slide_durations(video_path)
